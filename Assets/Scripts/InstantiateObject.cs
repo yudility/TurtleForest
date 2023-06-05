5bdf213a0f5b24d68e680781fafacb084a48909d
ethdrods <89960061+ethdrods@users.noreply.github.com>
2023-06-05 21:16:15 +0900

    Add files via upload

---
+++ Assets/Scripts/InstantiateObject.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateObject : MonoBehaviour
+{
+    public GameObject prefab;
+    public Terrain terrain;
+    public float yOffset = 0.2f;
+    public GameObject Basic;
+
+    private float terrainWidth;
+    private float terrainLength;
+
+    private float xTerrainPos;
+    private float zTerrainPos;
+    public int count = 0;
+
+
+    void Start()
+    {
+        //Get terrain size
+        terrainWidth = terrain.terrainData.size.x;
+        terrainLength = terrain.terrainData.size.z;
+
+        //Get terrain position
+        xTerrainPos = terrain.transform.position.x;
+        zTerrainPos = terrain.transform.position.z;
+
+        generateObjectOnTerrain();
+    }
+
+    void generateObjectOnTerrain()
+    {
+
+        //Generate random x,z,y position on the terrain
+        float randX = UnityEngine.Random.Range(xTerrainPos, xTerrainPos + terrainWidth);
+        float randZ = UnityEngine.Random.Range(zTerrainPos, zTerrainPos + terrainLength);
+        float yVal = Terrain.activeTerrain.SampleHeight(new Vector3(randX, 0, randZ));
+
+            //Apply Offset if needed
+        yVal = yVal + yOffset;
+
+        //Generate the Prefab on the generated position
+        //GameObject objInstance = (GameObject)Instantiate(prefab, new Vector3(randX, yVal, randZ), Quaternion.identity);
+        transform.position = new Vector3(randX - 1.0f, yVal, randZ - 1.0f);
+        Basic.transform.position = new Vector3(randX - 1.5f, yVal + 1.0f, randZ - 1.5f);
+
+    }
+
+    void Update()
+    {
+
+    }
+}
