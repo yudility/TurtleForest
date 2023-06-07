37227f7d203157d99539989d6765135174c4cce3
yudility <yudility@gmail.com>
2023-06-07 22:30:54 +0900

    Merge flashlight and watch to player & Copy player to map

---
+++ Assets/HPgauge.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPgauge : MonoBehaviour
+{
+    Image HPBar;
+    float maxHP = 300f;
+    public static float HP;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HPBar = GetComponent<Image>();
+        HP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HP -= Time.deltaTime;
+        HPBar.fillAmount = HP / maxHP;
+    }
+}
